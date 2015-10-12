706f9250f3f923e659c5d071c32e19af05dc52e7
robozachgithub <za.piazza@gmail.com>
2015-10-12 13:01:47 -0700

    new version using standard random number generator

--- plot_random.py
+++ plot_random.py
@@ -1,7 +1,7 @@
 # plot random numbers based on different generators
 
 import matplotlib.pyplot as plt
-
+import random
 
 
 #linear congruent random number generator
@@ -14,6 +14,8 @@ def rand_lin_con(length_of_random_sequence):
 	# m is the number from which to get the modulus
 	m = 112233
 
+
+
 	for i in range(length_of_random_sequence):
 		r2 = (a*r1 + c)%m
 		sqnc.append(r2)
@@ -23,27 +25,35 @@ def rand_lin_con(length_of_random_sequence):
 
 
 
+def rand_standard(length_of_random_sequence):
+
+	sqnc = []
+	for i in range(length_of_random_sequence):
+		sqnc.append(random.randint(0,250))
+	return sqnc
+
+
 def name_figure():
 	fig_title = raw_input("Type figure title withou extension, will be saves as *.pdf: ")
 	return fig_title
 
-sequence = rand_lin_con(300)
-
+#sequence = rand_lin_con(300)
+sequence = rand_standard(300)
 
 
 
 # plot pairs to reveal pattern
-x_points = []
-y_points = []
-for i in range(0,len(sequence),2):
-	x_points.append(sequence[i])
-	y_points.append(sequence[i+1])
+#x_points = []
+#y_points = []
+#for i in range(0,len(sequence),2):
+#	x_points.append(sequence[i])
+#	y_points.append(sequence[i+1])
 
 # make plot
-plt.plot(x_points, y_points, marker='.', linestyle='None')
-fig_title = name_figure()
-plt.savefig(fig_title)
-plt.show()
+#plt.plot(x_points, y_points, marker='.', linestyle='None')
+#fig_title = name_figure()
+#plt.savefig(fig_title)
+#plt.show()
 
 
 
